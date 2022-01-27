634c037f242d47f7333e5785754a802f2fac304e
Angie Cruz <16angie.c@gmail.com>
2022-01-26 21:36:38 -0500

    Se realiza la maquetación del media-player

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaPlayerComponent implements OnInit {
 
+  mockCover: any = {
+    cover: '',
+    album: 'Gioli',
+    name: 'BEBE (Oficial)',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
