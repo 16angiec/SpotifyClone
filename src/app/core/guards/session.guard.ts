04f18062fa8dbdd5d76ed536e382edc956112a53
Angie Lorena Cruz <acruz.ext@alianza.com.co>
2022-08-18 17:22:14 -0500

    Se agregan guards a la aplicacion

---
+++ src/app/core/guards/session.guard.ts
@@ -0,0 +1,32 @@
+import { CookieService } from 'ngx-cookie-service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionGuard implements CanActivate {
+
+  constructor(private cookie: CookieService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkCookieSession();
+  }
+
+  checkCookieSession(): boolean {
+    try {
+      const token: boolean = this.cookie.check('token')
+      if (!token) {
+        this.router.navigate(['/', 'auth'])
+      }
+      return token
+    } catch (e) {
+      console.log('Algo salio mal', e);
+      return false
+    }
+  }
+
+}
